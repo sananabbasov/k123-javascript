3cd20314e0de0861de7dca2bc464a92d217ee8cd
sananabbasov <sanan_abbasov@list.ru>
2023-03-16 12:05:25 +0400

    Functions

---
+++ Functions/main.js
@@ -0,0 +1,56 @@
+var myArr = ["Qudret", "Vusal"]
+
+
+// myArr.push("dasd")
+
+
+
+// function seoUrl(soz) {
+//     // soz = Git və Github nədir
+//     // soz = Leonardo da Vinçinin anası qafqazlı
+//     return soz.toLowerCase()
+//         .replaceAll(" ", "-")
+//         .replaceAll(".", "")
+//         .replaceAll("ə", "e")
+//         .replaceAll("ı", "i")
+//         .replaceAll("ç", "c")
+
+// }
+
+// console.log(seoUrl("Git və Github nədir"));
+// console.log(seoUrl("Leonardo da Vinçinin anası qafqazlı"));
+
+// var test = "Git və Github nədir. Aralarındakı fərqlər."
+
+// console.log(test);
+
+
+
+
+// console.log(test);
+
+// console.log(myArr);
+
+
+// function solution(str, ending) {
+//     var res = str.substring(str.length - ending.length)
+//     if (res == ending) {
+//         return true
+//     } 
+//     return false
+// }
+
+
+// console.log(solution("Murad", "ad")); 
+// console.log(solution("MehemmedEli", "i")); 
+
+
+function hesabla(reqem) {
+    
+}
+
+console.log(hesabla(5));
+
+
+// https://compar.edu.az/
+// www.compar.edu.az
